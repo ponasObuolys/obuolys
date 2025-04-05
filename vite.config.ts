a6f76b2bd8c86f988d9a04f97ce8ed97d1758b79
Aurimas Butvilauskas <67857119+ponasObuolys@users.noreply.github.com>
2025-04-05 13:52:58 +0300

    Problema "Unknown at rule @tailwind" buvo išspręsta

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import postcss from "postcss";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -18,5 +19,8 @@ export default defineConfig(({ mode }) => ({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  css: {
+    postcss: './postcss.config.cjs',
   },
 }));
